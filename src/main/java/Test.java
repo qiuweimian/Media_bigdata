caaeb9623c207d680443a4d05a631074a6cb1cee
yizhongxunsi <yizhongxunsi@gmail.com>
2018-09-07 13:55:09 +0800

    delete useless

--- src/main/java/Test.java
+++
@@ -1,8 +0,0 @@
-/**
- * Created by ch on 2018/8/17
- */
-public class Test {
-    public static void main(String[] args) {
-        System.out.println("Hello World");
-    }
-}
--- src/main/scala/Test1.scala
+++
@@ -1,8 +0,0 @@
-/**
-  * Created by ch on 2018/8/17
-  */
-object Test1 {
-  def main(args: Array[String]): Unit = {
-    println("Hello World")
-  }
-}
